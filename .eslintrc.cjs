d6ad5c3f012a0daa54a86c0a58fb609c1bfcc5e6
Jacek Eichler <jacekeichler@gmail.com>
2023-05-22 19:04:50 +0200

    Generate lighthouse audit with throttling disabled

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: false,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  }
+}
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello world !')
+import fs from 'fs'
+import puppeteer from 'puppeteer'
+import { startFlow } from 'lighthouse'
+
+async function captureReport () {
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+
+  const flow = await startFlow(page,
+
+    {
+      config: {
+        extends: 'lighthouse:default',
+        settings: {throttlingMethod: 'provided'}
+      }
+    })
+  await flow.navigate('http://localhost:4200')
+
+  await browser.close()
+  fs.writeFileSync('report.html', await flow.generateReport())
+  fs.writeFileSync('flow-result.json', JSON.stringify(await flow.createFlowResult(), null, 2))
+}
+
+captureReport()
