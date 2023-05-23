fb010e73bc7c3d66d4e7e53735b2f98f9e503cab
Jacek Eichler <jacekeichler@gmail.com>
2023-05-23 17:14:08 +0200

    Started saving results to separate file

--- index.js
+++ index.js
@@ -32,3 +32,40 @@ async function captureReport () {
 }
 
 captureReport()
+
+async function saveResultsInJson (testVariant) {
+  const defaultDir = 'X:/iCloudDrive/Studies/Studia_magisterskie/Praca magisterksa/Lighthouse - automatic tests'
+  const dirToSave = defaultDir + `/${testVariant}`
+
+  if (!fs.existsSync(dirToSave)) {
+    fs.mkdirSync(dirToSave, { recursive: true })
+  }
+
+
+
+  const 
+
+  const dataToSave = JSON.stringify(student)
+  const fileDir = dirToSave + '/deployed_1.json'
+
+  if (fs.existsSync(fileDir)) {
+    console.log('jacek :>> ')
+  } else {
+    const dataToSave = {
+      'first-contentful-paint': [],
+      "largest-contentful-paint": [],
+      "speed-index": [],
+      "total-blocking-time": [],
+      "cumulative-layout-shift": [],
+      "performance": [],
+      "accessibility": [],
+      "best-practices": [],
+      "seo": [],
+      "pwa": []
+    }
+
+    fs.writeFileSync(fileDir, dataToSave)
+  }
+}
+
+saveResultsInJson('initial-version')
