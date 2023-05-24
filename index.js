af81f0fee11384a79df1e280342c2a0c6b1c34df
Jacek Eichler <jacekeichler@gmail.com>
2023-05-24 13:53:02 +0200

    Saving proper information in the json, which aggregates data from audits

--- index.js
+++ index.js
@@ -1,39 +1,7 @@
 import fs from 'fs'
 import captureReport from './captureReport.js'
+import saveResultsInJson from './saveResults.js'
 
-// async function saveResultsInJson (testVariant) {
-//   const defaultDir = 'X:/iCloudDrive/Studies/Studia_magisterskie/Praca magisterksa/Lighthouse - automatic tests'
-//   const dirToSave = defaultDir + `/${testVariant}`
+saveResultsInJson('initial_version', 2)
 
-//   if (!fs.existsSync(dirToSave)) {
-//     fs.mkdirSync(dirToSave, { recursive: true })
-//   }
-
-//   const
-
-//   const dataToSave = JSON.stringify(student)
-//   const fileDir = dirToSave + '/deployed_1.json'
-
-//   if (fs.existsSync(fileDir)) {
-//     console.log('jacek :>> ')
-//   } else {
-//     const dataToSave = {
-//       'first-contentful-paint': [],
-//       "largest-contentful-paint": [],
-//       "speed-index": [],
-//       "total-blocking-time": [],
-//       "cumulative-layout-shift": [],
-//       "performance": [],
-//       "accessibility": [],
-//       "best-practices": [],
-//       "seo": [],
-//       "pwa": []
-//     }
-
-//     fs.writeFileSync(fileDir, dataToSave)
-//   }
-// }
-
-// saveResultsInJson('initial-version')
-
-captureReport('http://localhost:4200', 'initial_version', 3)
+// captureReport('http://localhost:4200', 'initial_version', 3)
---
+++ saveResults.js
@@ -0,0 +1,80 @@
+import fs from 'fs'
+
+export default async function saveResultsInJson (testVariant, index) {
+  const defaultDir = 'X:/iCloudDrive/Studies/Studia_magisterskie/Praca magisterksa/Lighthouse - automatic tests'
+  const dirToSave = defaultDir + `/${testVariant}/`
+
+  if (!fs.existsSync(dirToSave)) {
+    fs.mkdirSync(dirToSave, { recursive: true })
+  }
+
+  const jsonFileLocation = dirToSave + `${testVariant}_${index}.json`
+
+  const jsonRawData = fs.readFileSync(jsonFileLocation)
+
+  const json = JSON.parse(jsonRawData)
+
+  const fileDir = dirToSave + `${testVariant}_aggregated_results.json`
+
+  if (fs.existsSync(fileDir)) {
+    const aggregatedJsonRawData = fs.readFileSync(fileDir)
+
+    const aggregatedJson = JSON.parse(aggregatedJsonRawData)
+    aggregatedJson['cold-load']['first-contentful-paint'].push(json.steps[0].lhr.audits['first-contentful-paint'])
+    aggregatedJson['cold-load']['largest-contentful-paint'].push(json.steps[0].lhr.audits['largest-contentful-paint'])
+    aggregatedJson['cold-load']['speed-index'].push(json.steps[0].lhr.audits['speed-index'])
+    aggregatedJson['cold-load']['total-blocking-time'].push(json.steps[0].lhr.audits['total-blocking-time'])
+    aggregatedJson['cold-load']['cumulative-layout-shift'].push(json.steps[0].lhr.audits['cumulative-layout-shift'])
+    aggregatedJson['cold-load'].performance.push(json.steps[0].lhr.categories.performance.score)
+    aggregatedJson['cold-load'].accessibility.push(json.steps[0].lhr.categories.accessibility.score)
+    aggregatedJson['cold-load']['best-practices'].push(json.steps[0].lhr.categories['best-practices'].score)
+    aggregatedJson['cold-load'].seo.push(json.steps[0].lhr.categories.seo.score)
+    aggregatedJson['cold-load'].pwa.push(json.steps[0].lhr.categories.pwa.score)
+
+    aggregatedJson['warm-load']['first-contentful-paint'].push(json.steps[1].lhr.audits['first-contentful-paint'])
+    aggregatedJson['warm-load']['largest-contentful-paint'].push(json.steps[1].lhr.audits['largest-contentful-paint'])
+    aggregatedJson['warm-load']['speed-index'].push(json.steps[1].lhr.audits['speed-index'])
+    aggregatedJson['warm-load']['total-blocking-time'].push(json.steps[1].lhr.audits['total-blocking-time'])
+    aggregatedJson['warm-load']['cumulative-layout-shift'].push(json.steps[1].lhr.audits['cumulative-layout-shift'])
+    aggregatedJson['warm-load'].performance.push(json.steps[1].lhr.categories.performance.score)
+    aggregatedJson['warm-load'].accessibility.push(json.steps[1].lhr.categories.accessibility.score)
+    aggregatedJson['warm-load']['best-practices'].push(json.steps[1].lhr.categories['best-practices'].score)
+    aggregatedJson['warm-load'].seo.push(json.steps[1].lhr.categories.seo.score)
+    aggregatedJson['warm-load'].pwa.push(json.steps[1].lhr.categories.pwa.score)
+
+    const jsonToSave = JSON.stringify(aggregatedJson)
+
+    fs.writeFileSync(fileDir, jsonToSave)
+  } else {
+    const dataToSave = {
+      'cold-load': {
+        'first-contentful-paint': [json.steps[0].lhr.audits['first-contentful-paint']],
+        'largest-contentful-paint': [json.steps[0].lhr.audits['largest-contentful-paint']],
+        'speed-index': [json.steps[0].lhr.audits['speed-index']],
+        'total-blocking-time': [json.steps[0].lhr.audits['total-blocking-time']],
+        'cumulative-layout-shift': [json.steps[0].lhr.audits['cumulative-layout-shift']],
+        performance: [json.steps[0].lhr.categories.performance.score],
+        accessibility: [json.steps[0].lhr.categories.accessibility.score],
+        'best-practices': [json.steps[0].lhr.categories['best-practices'].score],
+        seo: [json.steps[0].lhr.categories.seo.score],
+        pwa: [json.steps[0].lhr.categories.pwa.score]
+      },
+      'warm-load': {
+        'first-contentful-paint': [json.steps[1].lhr.audits['first-contentful-paint']],
+        'largest-contentful-paint': [json.steps[1].lhr.audits['largest-contentful-paint']],
+        'speed-index': [json.steps[1].lhr.audits['speed-index']],
+        'total-blocking-time': [json.steps[1].lhr.audits['total-blocking-time']],
+        'cumulative-layout-shift': [json.steps[1].lhr.audits['cumulative-layout-shift']],
+        performance: [json.steps[1].lhr.categories.performance.score],
+        accessibility: [json.steps[1].lhr.categories.accessibility.score],
+        'best-practices': [json.steps[1].lhr.categories['best-practices'].score],
+        seo: [json.steps[1].lhr.categories.seo.score],
+        pwa: [json.steps[1].lhr.categories.pwa.score]
+      }
+
+    }
+    const jsonToSave = JSON.stringify(dataToSave)
+
+    fs.writeFileSync(fileDir, jsonToSave)
+  }
+}
